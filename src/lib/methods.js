0ad97479f2f0e83dd970269731ed9650c5d22315
ada-u <yuta@adachi.life>
2019-12-08 22:42:32 +0900

    Remove base64uploader

--- src/lib/methods.js
+++ src/lib/methods.js
@@ -4,43 +4,6 @@ import 'firebase/storage';
 import sortBy from 'sort-by';
 import { CREATE } from 'react-admin';
 
-const convertFileToBase64 = file =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file.rawFile);
-
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = reject;
-  });
-
-const addUploadFeature = requestHandler => (type, resource, params) => {
-  if (type === 'UPDATE') {
-    if (params.data.image && params.data.image.length) {
-      const formerPictures = params.data.image.filter(p => !(p.rawFile instanceof File));
-      const newPictures = params.data.image.filter(p => p.rawFile instanceof File);
-
-      return Promise.all(newPictures.map(convertFileToBase64))
-        .then(base64Pictures =>
-          base64Pictures.map(picture64 => ({
-            src: picture64,
-            title: `${params.data.title}`
-          }))
-        )
-        .then(transformedNewPictures =>
-          requestHandler(type, resource, {
-            ...params,
-            data: {
-              ...params.data,
-              image: [...transformedNewPictures, ...formerPictures]
-            }
-          })
-        );
-    }
-  }
-  // for other request types and reources, fall back to the defautl request handler
-  return requestHandler(type, resource, params);
-};
-
 const getImageSize = file => {
   return new Promise(resolve => {
     const img = document.createElement('img');
@@ -289,6 +252,5 @@ export default {
   getOne,
   getList,
   getMany,
-  getManyReference,
-  addUploadFeature
+  getManyReference
 };
